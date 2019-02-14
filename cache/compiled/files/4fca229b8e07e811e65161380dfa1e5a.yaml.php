bb3473789a3c336bc4b9adb8df7c4b6391348575
Phoenix Eve Aspacio <aspaciop@gmail.com>
2019-02-14 20:19:13 +0800

    cleanup

--- cache/compiled/files/4fca229b8e07e811e65161380dfa1e5a.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/documentation/user/config/security.yaml',
-    'modified' => 1550129478,
-    'data' => [
-        'salt' => 'lzMuAfQkZTY2i4'
-    ]
-];
--- cache/compiled/files/b45bb661f88041efa961ef9c6a47eef4.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/documentation/user/config/plugins/highlight.yaml',
-    'modified' => 1549581056,
-    'data' => [
-        'theme' => 'learn'
-    ]
-];
